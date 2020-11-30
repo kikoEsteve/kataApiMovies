0d0de5b905bb36923e1c2f145f33ed5b575df961
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-30 04:10:42 +0100

    README & some changes in Header.css trying to custom title font

--- kata-api-movies/src/components/Header.jsx
+++ kata-api-movies/src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <h1>netflixClon</h1>
+                <h3>netflixClon</h3>
             </div>
         )
     }
