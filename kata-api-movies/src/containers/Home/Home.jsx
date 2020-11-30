a5f52c02eb13eaf4120e3a85c27a03b17293005a
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-30 03:24:46 +0100

    Home added

--- kata-api-movies/src/containers/Home/Home.jsx
+++ kata-api-movies/src/containers/Home/Home.jsx
@@ -1,49 +1,66 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import './Home.css';
 import axios from 'axios';
+import MovieComponent from '../../components/MovieComponent';
 
-class Home extends React.Component {
+class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            theMovieDB: []
+            movies: [],
+            page: 1
         }
     };
+    //Accedemos a la api via axios usando el componentDidMount
+    componentDidMount() {
+        axios.get(`https://api.themoviedb.org/3/movie/` + `popular?api_key=2f0ee4659e75f22b0645bb580847d157&language=en-US&page=${this.state.page}`)
+            .then((api) => {
+                this.setState({ movies: api.data.results })
+            })
+    } catch(err) {
+        console.log(err);
+    };
+    //Si queremos que los cambios realizados se pinten en la app, debemos actualizar. Ejemplos: previousPage, nextPage..
+    componentDidUpdate() {
+        axios.get(`https://api.themoviedb.org/3/movie/` + `popular?api_key=2f0ee4659e75f22b0645bb580847d157&language=en-US&page=${this.state.page}`)
+            .then((api) => {
+                this.setState({ movies: api.data.results })
+            })
+    } catch(err) {
+        console.log(err);
+    };
 
-    async componentDidMount() {
-        try {
-            const response = await axios.get('https://api.themoviedb.org/3/movie/550?api_key=2f0ee4659e75f22b0645bb580847d157');
-            console.log(response.data.results);
-            this.setState({ theMovieDB: response.data.results })
-        } catch (err) {
-            console.log(err);
-        }
-    }
+    //Añadimos la lógica para cambiar de página anterior
+    previousPage = () => {
+        this.setState({ page: this.state.page - 1, })
+        console.log(this.state.page)
+    };
+
+    //Añadimos la lógica para cambiar a la página siguiente
+    nextPage = () => {
+        this.setState({ page: this.state.page + 1, })
+    };
 
     showResults() {
-        if (this.state.theMovieDB[""]) {
-            return (
-                this.state.theMovieDB.map(movie => {
-                    return (
-                        <div className="movie" key={movie.id}>
-                            {movie.id}
-                            <img onClick={() => this.clickSelectedElement(movie)} alt={movie.name} src={movie.image}></img>
-                            {movie.release_date}
-                        </div>
-                    )
-                })
-            )
-        } else {
-            return (
-                <div>LOADING..</div>
-            )
-        }
-    }
+        return (
+            <Fragment>
+                <div>
+                    <button onClick={this.previousPage}> Anterior </button>
+                    <button onClick={this.nextPage}> Siguiente </button>
+                </div>
+                <div className="general">
+                    {
+                        this.state.movies.map(item => <MovieComponent item={item} history={this.props.history} />)
+                    }
+                </div>
+            </Fragment>
+        )
+    };
 
     clickSelectedElement(movie) {
-        this.props.history.push('/Moviedetail');
+        this.props.history.push('./Moviedetail');
         localStorage.setItem('movieData', JSON.stringify(movie));
-    }
+    };
 
     render() {
         return (
